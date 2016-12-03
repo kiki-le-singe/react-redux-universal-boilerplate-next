f373272becf29031ee180cd8971c0cb6897a51ec
kiki le singe <aalbertini95@gmail.com>
2016-12-03 01:28:55 +0100

    chore(setup): add semicolon to the end scripts

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,18 +6,18 @@ select yn in "Yes" "No" "Cancel"; do
     Yes )
       echo "All SASS (styles, config) files are going to be deleted";
 
-      rm -rf webpack-assets.json postcss.config.js webpack/dev.config.js webpack/isomorphic.tools.config.js src/common/layouts/AppLayout/index.js src/common/styles src/common/views/AboutView/AboutView.jsx
-      mv postcss.config.cssnext.js postcss.config.js
-      mv webpack/dev.config.cssnext.js webpack/dev.config.js
-      mv webpack/isomorphic.tools.config.cssnext.js webpack/isomorphic.tools.config.js
-      mv src/common/layouts/AppLayout/index.cssnext.js src/common/layouts/AppLayout/index.js
-      mv src/common/styles.cssnext src/common/styles
-      mv src/common/views/AboutView/AboutView.cssnext.jsx src/common/views/AboutView/AboutView.jsx
-      mv src/common/views/AboutView/AboutView.scss src/common/views/AboutView/AboutView.css
+      rm -rf webpack-assets.json postcss.config.js webpack/dev.config.js webpack/isomorphic.tools.config.js src/common/layouts/AppLayout/index.js src/common/styles src/common/views/AboutView/AboutView.jsx;
+      mv postcss.config.cssnext.js postcss.config.js;
+      mv webpack/dev.config.cssnext.js webpack/dev.config.js;
+      mv webpack/isomorphic.tools.config.cssnext.js webpack/isomorphic.tools.config.js;
+      mv src/common/layouts/AppLayout/index.cssnext.js src/common/layouts/AppLayout/index.js;
+      mv src/common/styles.cssnext src/common/styles;
+      mv src/common/views/AboutView/AboutView.cssnext.jsx src/common/views/AboutView/AboutView.jsx;
+      mv src/common/views/AboutView/AboutView.scss src/common/views/AboutView/AboutView.css;
 
       echo "This setup (bin/setup.sh) will self-destruct 💥 .";
 
-      rm -rf bin/setup.sh
+      rm -rf bin/setup.sh;
 
       echo "Congratulations you are going to use CSSNEXT 👏 .";
 
@@ -26,11 +26,11 @@ select yn in "Yes" "No" "Cancel"; do
     No )
       echo "All CSSNEXT (styles, config) files are going to be deleted";
 
-      rm -rf webpack-assets.json postcss.config.cssnext.js webpack/dev.config.cssnext.js webpack/isomorphic.tools.config.cssnext.js src/common/layouts/AppLayout/index.cssnext.js src/common/styles.cssnext src/common/views/AboutView/AboutView.cssnext.jsx
+      rm -rf webpack-assets.json postcss.config.cssnext.js webpack/dev.config.cssnext.js webpack/isomorphic.tools.config.cssnext.js src/common/layouts/AppLayout/index.cssnext.js src/common/styles.cssnext src/common/views/AboutView/AboutView.cssnext.jsx;
 
       echo "This setup (bin/setup.sh) will self-destruct 💥 .";
 
-      rm -rf bin/setup.sh
+      rm -rf bin/setup.sh;
 
       echo "Congratulations you are going to use SASS 👏 .";
 
