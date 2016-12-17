9577eee87e85352ff8cd1d6003678f92034a02ff
kiki le singe <aalbertini95@gmail.com>
2016-12-17 17:40:57 +0100

    feat(Html): transform Html as component

---
+++ src/server/components/Html/index.jsx
@@ -0,0 +1,69 @@
+import React, { Component, PropTypes } from 'react'
+import { renderToString } from 'react-dom/server'
+import Helmet from 'react-helmet'
+
+export default class Html extends Component {
+  static propTypes = {
+    assets: PropTypes.object,
+    component: PropTypes.node,
+    store: PropTypes.object
+  }
+
+  get scripts() {
+    const { javascript } = this.props.assets
+
+    return Object.keys(javascript).reverse().map((script, i) =>
+      <script src={javascript[script]} key={i} />
+    )
+  }
+
+  get styles() {
+    const { assets } = this.props
+    const { styles, assets: _assets } = assets
+    const stylesArray = Object.keys(styles)
+
+    // styles (will be present only in production with webpack extract text plugin)
+    if (stylesArray.length !== 0) {
+      return stylesArray.map((style, i) =>
+        <link href={assets.styles[style]} key={i} rel="stylesheet" type="text/css" />
+      )
+    }
+
+    // (will be present only in development mode)
+    // It's not mandatory but recommended to speed up loading of styles
+    // (resolves the initial style flash (flicker) on page load in development mode)
+    const scssPaths = Object.keys(_assets).filter(asset => asset.includes('.css') || asset.includes('.scss'))
+    return scssPaths.map((style, i) =>
+      <style dangerouslySetInnerHTML={{ __html: _assets[style]._style }} key={i} />
+    )
+  }
+
+  render() {
+    const { component, store } = this.props
+    const head = Helmet.rewind()
+    const htmlAttributes = head.htmlAttributes.toComponent()
+
+    return (
+      <html {...htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+
+          {head.title.toComponent()}
+          {head.meta.toComponent()}
+          {head.link.toComponent()}
+          {head.script.toComponent()}
+
+          {this.styles}
+        </head>
+
+        <body>
+          <div id="root" dangerouslySetInnerHTML={{ __html: renderToString(component) }} />
+          <script dangerouslySetInnerHTML={{ __html: `window.__PRELOADED_STATE__=${JSON.stringify(store.getState())};` }} />
+          {this.scripts}
+        </body>
+      </html>
+    )
+  }
+}
